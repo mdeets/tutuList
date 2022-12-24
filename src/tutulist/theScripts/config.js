180de143674a55eba502cd4bb81b9698727dad0e
mrx <albin@senioradministrator.com>
2022-12-24 21:55:43 +0100

    the table is updated, i donw know why my deleteTable function isnt work so i have to rename table after each table upgrades

--- src/tutulist/theScripts/config.js
+++ src/tutulist/theScripts/config.js
@@ -23,7 +23,7 @@ const color_unknown = "white";
 //database Tables
 
 const table_allTasks = "tutu_allTasks1";
-const table_completedTasks = "tutu_completedTasks1";
-const table_todayTasks = "tutu_todayTasks1";
+const table_completedTasks = "tutu_completedTasks3";
+const table_todayTasks = "tutu_todayTasks2";
 const table_taskSteps = "tutu_taskSteps1";
 const table_settings = "tutu_settings1";
