16a9a55dc829a8339d6470c0753a6845f8461a6f
mrx <albin@senioradministrator.com>
2023-01-10 03:10:08 +0100

    some ui links placed

--- src/tutulist/theScripts/config.js
+++ src/tutulist/theScripts/config.js
@@ -51,3 +51,141 @@ const icon_backward = pathToIcon + "24x24/"+"backward.png"; //https://icons8.com
 
 //alltasks -> add new step
 const icon_cancel = pathToIcon+"24x24/"+"cancel.png";
+
+
+/*
+  user interface idee
+
+  https://dribbble.com/shots/17777364-Productivity-Mobile-App-iOS-Android-UI
+
+
+
+  bottom indicator , search box , back button
+  https://dribbble.com/shots/19153569-Mobile-App-iOS-Android-UI
+
+  theme colors , back button, top-left open menu, settings page
+  https://dribbble.com/shots/16540622-Finance-App-Dark-Theme
+
+
+  bottom indicator
+  https://dribbble.com/shots/17592730-Mental-Health-App
+
+  tags idee for icon_addTasks
+  https://dribbble.com/shots/20127375-ToDo-List
+
+  theme colors.
+  https://dribbble.com/shots/17795308-Attio-Mobile-Navigation
+
+
+  calendar style
+  https://dribbble.com/shots/18181992-Streak
+
+
+  task steps style
+  https://dribbble.com/shots/15829912-Done-Create-Task
+
+  mini menu to do actions for task
+  https://dribbble.com/shots/15726170-Navigation-panel
+
+
+
+  search box
+  https://dribbble.com/shots/16473464-Task-Management-App
+
+
+  calendar style
+  https://dribbble.com/shots/15829938-Done-Date-Picker
+
+
+
+
+  goals idee
+  https://dribbble.com/shots/15794848--Done-Goals
+
+
+  show stats and categorization idee
+  https://dribbble.com/shots/18627890-Task-Management-App-Design
+
+
+  forms -> text inputs style, check box style, combo style.
+  https://dribbble.com/shots/14794406-Task-detail-desktop-app
+
+
+  show priority status idee/style , task steps style
+  https://dribbble.com/shots/15829896-Done-To-Do-List
+
+
+  background color
+  https://dribbble.com/shots/18468605-Task-Management-App
+
+
+
+  idee for texts in completed tasks
+  https://dribbble.com/shots/19752197-Task-Management-App
+
+
+  bg color and calender style and chart style
+  https://dribbble.com/shots/17339848-Managy-Task-Manager-Dashboard
+
+
+  purple color, buttons, serch box
+  https://dribbble.com/shots/15182650-Dashboard-Mobile-View-Schedule
+
+
+
+  show date style
+  https://dribbble.com/shots/20159263-Task-and-Project-Management-Mobile-App
+
+  responsive idee
+  https://dribbble.com/shots/17259558-Todoist-Material-Design-Award-Winner-2021
+
+
+
+  dashboard idee, form idee,
+  https://dribbble.com/shots/19923658-Task-list-mobile-app
+
+
+  login idee
+  https://dribbble.com/shots/14425035-Awayday-Travel-App-Daily-UI-Challenge-001-Sign-up-flow
+
+  show notifications
+  https://dribbble.com/shots/16891665-Daily-UI-011-Flash-Message
+
+
+
+  settings, theme colors
+  https://dribbble.com/shots/14630755-Cards-Dark-UI
+  https://dribbble.com/shots/14510224-Cards-White-UI
+  https://dribbble.com/shots/17873430-Appearance-Settings
+  https://dribbble.com/shots/16838613-Dark-Light-UI-for-Cards-Components
+  https://dribbble.com/shots/15375254-End-User-Profile-Settings
+  https://dribbble.com/shots/11023100-Daily-UI-006-007-Profile-Settings
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  */
